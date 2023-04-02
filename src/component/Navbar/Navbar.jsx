d1cc526cb98d1118f13793e160469733230883c9
Aminul667 <rahathssn@gmail.com>
2023-04-02 15:13:31 +0600

    53-3 Responsive Navbar using react and tailwind

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import Link from "../Link/Link";
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false); useState
+
   const routes = [
     { id: 1, name: "Home", path: "/" },
     { id: 2, name: "About", path: "/about" },
@@ -11,9 +15,15 @@ const Navbar = () => {
 
   return (
     <nav>
-        <ul>
+        <div onClick={() => setOpen(!open)}>
+            <Bars3Icon className="h-6 w-6 text-purple-600" />
+        </div>
+        <ul className="md:flex">
             {
-                routes.map(route => <li>{route.name}</li>)
+                routes.map(route => <Link
+                key={route.id}
+                route={route}
+                ></Link>)
             }
         </ul>
     </nav>
